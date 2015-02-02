7b23a9840af7d65dd176c366e266b1b75a710d03
cwachowicz <chris@localz.co>
2015-02-02 12:01:48 +0000

    First build.

---
+++ server.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+var routes = require('./routes');
+
+var config = { };
+var server = new Hapi.Server('0.0.0.0', 8080, config);
+server.pack.require({ lout: { endpoint: '/docs' } }, function (err) {
+
+    if (err) {
+        console.log('Failed loading plugins');
+    }
+});
+
+server.addRoutes(routes);
+
+server.start();
