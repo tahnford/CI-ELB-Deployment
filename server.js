3576e144cdb7a83db0ff945e9d3dfcffd0039abd
cwachowicz <chris@localz.co>
2015-02-02 12:15:00 +0000

    Updated Travis secure keys.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 var routes = require('./routes');
 
 var config = { };
-var server = new Hapi.Server('0.0.0.0', 8080, config);
+var server = new Hapi.Server('0.0.0.0', 3000, config);
 server.pack.require({ lout: { endpoint: '/docs' } }, function (err) {
 
     if (err) {
